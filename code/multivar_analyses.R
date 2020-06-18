99b203f07326e209cb4dafd7f46ec00deedc5488
Cole B. Brookson <brookson@ualberta.ca>
2020-06-18 02:53:08 -0600

    final versions of plots for both diet and for vulnerability

--- code/multivar_analyses.R
+++ code/multivar_analyses.R
@@ -49,19 +49,19 @@ summary(nmds_jaccard_k2)
 stressplot(nmds_jaccard_k2)
 nmds_jaccard_k2$stress
 
-nmds_k2_vis_data = data.frame(scores(nmds_jaccard_k2)) 
-nmds_k2_vis_data$species = rownames(nmds_k2_vis_data)  
-nmds_k2_vis_data$vulner_score = grouping_data$cat_vulnerab_score  
-nmds_k2_vis_data$diet_study = grouping_data$diet_study 
+## make plot for vulnerability
+nmds_k2_vis_data_vulner = data.frame(scores(nmds_jaccard_k2)) 
+nmds_k2_vis_data_vulner$species = rownames(nmds_k2_vis_data_vulner)  
+nmds_k2_vis_data_vulner$vulner_score = grouping_data$cat_vulnerab_score  
 
-split_nmds_k2 = split(nmds_k2_vis_data, nmds_k2_vis_data$vulner_score)
-applied_nmds_k2 = lapply(split_nmds_k2, function(df){
+split_nmds_k2_vulner = split(nmds_k2_vis_data_vulner, nmds_k2_vis_data_vulner$vulner_score)
+applied_nmds_k2_vulner = lapply(split_nmds_k2_vulner, function(df){
   df[chull(df), ]
 })
-combined_nmds_k2 = do.call(rbind, applied_nmds_k2)
+combined_nmds_k2_vulner = do.call(rbind, applied_nmds_k2_vulner)
 
-## make plot for vulnerability
-nmds_k2_plot <- ggplot(data=combined_nmds_k2) + 
+
+nmds_k2_vulner_plot <- ggplot(data=combined_nmds_k2_vulner) + 
   geom_polygon(aes(x = NMDS1, y = NMDS2, fill= fct_relevel(vulner_score, 'Low', 'Med', 'High'), 
                    group=fct_relevel(vulner_score, 'Low', 'Med', 'High')), alpha=0.30) + # add the convex hulls
   geom_point(aes(x=NMDS1,y=NMDS2, colour = fct_relevel(vulner_score, 'Low', 'Med', 'High')), size=2) + # add the point markers
@@ -75,74 +75,44 @@ nmds_k2_plot <- ggplot(data=combined_nmds_k2) +
         legend.title = element_text(size = 18), 
         legend.text = element_text(size = 18),
         legend.justification = c(0,0.5),
-        #panel.background = element_rect(fill = "lightgrey"), 
         panel.grid.major = element_blank(),  #remove major-grid labels
         panel.grid.minor = element_blank(),  #remove minor-grid labels
         plot.background = element_blank()) +
   scale_fill_manual('Vulnerability', values = c('#006994', '#ffd300', '#ca3433')) +
   scale_colour_manual('Vulnerability', values = c('#006994', '#ffd300', '#ca3433'))
-  #guides(fill = FALSE, colour = FALSE) +
-  #guides(fill = guide_legend(order = 1), colour = guide_legend(order = 2)) +
-  #scale_fill_viridis(option="magma", discrete = TRUE, begin = 0.8, end = 0.2, name = "Vulernability") + #name = "Global Change Assessed"
-  #scale_colour_viridis(option="magma", discrete = TRUE, begin = 0.8, end = 0.2, name = "Vulernability") 
+ 
+## make plot for vulnerability
+nmds_k2_vis_data_diet = data.frame(scores(nmds_jaccard_k2)) 
+nmds_k2_vis_data_diet$species = rownames(nmds_k2_vis_data_diet)  
+nmds_k2_vis_data_diet$diet_study = grouping_data$diet_study 
 
-treat=nmds_k2_vis_data$vulner_score
-ordiplot(nmds_jaccard_k2,type="n")
-ordihull(nmds_jaccard_k2,groups=treat,draw="polygon",col=c('red', 'blue', 'green'),label=F, alpha = 0.4)
-legend(x="bottomleft", legend=levels(nmds_k2_vis_data$vulner_score), col=c('red', 'blue', 'green'))
-orditorp(nmds_jaccard_k2,display="species",col="red",air=0.01)
-orditorp(nmds_jaccard_k2,display="sites",col=c(rep("green",5),rep("blue",5)),
-         air=0.01,cex=1.25)
+split_nmds_k2_diet = split(nmds_k2_vis_data_diet, nmds_k2_vis_data_diet$diet_study)
+applied_nmds_k2_diet = lapply(split_nmds_k2_diet, function(df){
+  df[chull(df), ]
+})
+combined_nmds_k2_diet = do.call(rbind, applied_nmds_k2_diet)
+combined_nmds_k2_diet = combined_nmds_k2_diet %>% 
+  mutate(diet_cat = ifelse(combined_nmds_k2_diet$diet_study == 1, 'Present', 'Absent'))
 
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-ggplot(data = nmds_k2_vis_data) +
-  geom_polygon(aes(x = NMDS1, y = NMDS2, fill = vulner_score)) +
-  geom_text(aes(x = NMDS1, y = NMDS2, label = species), alpha = 0.5)
-
-ggplot() + 
-  geom_polygon(nmds_k2_vis_data, aes(x = NMDS1,
-                                     y = NMDS2, fill = vulner_score)),
-                                     , alpha = 0.30) + # add the convex hulls
-  geom_text(nmds_k2_vis_data, aes(x = NMDS1, 
-                                  y = NMDS2,label = species), 
-            alpha=0.5) +  # add the species labels
-  geom_point(nmds_vis_data, aes(x = NMDS1, 
-                                y = NMDS2, shape = grp, colour = grp),
-             size = 4) + # add the point markers
-  scale_colour_manual(values=c("A" = "red", "B" = "blue")) +
+nmds_k2_plot_diet <- ggplot(data=combined_nmds_k2_diet) + 
+  geom_polygon(aes(x = NMDS1, y = NMDS2, fill= fct_relevel(diet_cat, 'Absent', 'Present'), 
+                   group=fct_relevel(diet_cat, 'Absent', 'Present')), alpha=0.30) + # add the convex hulls
+  geom_point(aes(x=NMDS1,y=NMDS2, colour = diet_cat), size=2) + # add the point markers
   coord_equal() +
-  theme_bw()
+  theme_bw()  +
+  theme(axis.text.x = element_blank(),  # remove x-axis text
+        axis.text.y = element_blank(), # remove y-axis text
+        axis.ticks = element_blank(),  # remove axis ticks
+        axis.title.x = element_text(size=18), # remove x-axis labels
+        axis.title.y = element_text(size=18), # remove y-axis labels
+        legend.title = element_text(size = 18), 
+        legend.text = element_text(size = 18),
+        legend.justification = c(0,0.5),
+        panel.grid.major = element_blank(),  #remove major-grid labels
+        panel.grid.minor = element_blank(),  #remove minor-grid labels
+        plot.background = element_blank()) +
+  scale_fill_manual('Presence in Diet Studies', values = c('#000080', '#ed2939')) +
+  scale_colour_manual('Presence in Diet Studies', values = c('#000080', '#ed2939'))
 
 
 
@@ -150,6 +120,7 @@ ggplot() +
 
 
 
+#################################### repear mds with 3d, but I don't think we'll use it
 nmds_jaccard_k3 = metaMDS(traits_data,
                        distance = 'jaccard',
                        trymax = 100,
@@ -164,107 +135,4 @@ nmds_vis_data = data.frame(scores(nmds_jaccard))
 nmds_vis_data$species = rownames(nmds_vis_data)  
 nmds_vis_data$vulner_score = grouping_data$cat_vulnerab_score  
 nmds_vis_data$diet_study = grouping_data$diet_study 
-
-
-ggplot() + 
-  geom_polygon(nmds_vis_data,aes(x = NMDS1,y = NMDS2,fill=grp,group=grp),alpha=0.30) + # add the convex hulls
-  geom_text(nmds_vis_data,aes(x=NMDS1,y=NMDS2,label=species),alpha=0.5) +  # add the species labels
-  geom_point(nmds_vis_data,aes(x=NMDS1,y=NMDS2,shape=grp,colour=grp),size=4) + # add the point markers
-  scale_colour_manual(values=c("A" = "red", "B" = "blue")) +
-  coord_equal() +
-  theme_bw()
-
-
-plot(nmds_jaccard)
-ordiplot(nmds_jaccard, type = 'p')
-group = unique(grouping_data$cat_vulnerab_score)
-for(i in unique(grouping_data$cat_vulnerab_score)) {
-  ordihull(nmds_jaccard$point[grep(i, group),], draw="polygon",
-           groups = group[group == i],col = colors[grep(i,group)],label=F) 
-}
-orditorp(nmds_jaccard, display = "species", col = "red", air = 0.01)
-orditorp(nmds_jaccard, display = "sites", col = c(rep("red",12),
-                                                  rep("blue", 12)), air = 0.01, cex = 1.25)
-
-
-
-
-
-data("varespec")
-
-varespec %>%
-  metaMDS(trace = F) %>%
-  ordiplot(type = "none") %>%
-  text("sites")
-
-dist <- vegdist(varespec,  method = "bray")
-
-# In this part, we define a function NMDS.scree() that automatically 
-# performs a NMDS for 1-10 dimensions and plots the nr of dimensions vs the stress
-NMDS.scree <- function(x) { #where x is the name of the data frame variable
-  plot(rep(1, 10), replicate(10, metaMDS(x, autotransform = F, k = 1)$stress), 
-       xlim = c(1, 10),ylim = c(0, 0.30), xlab = "# of Dimensions", 
-       ylab = "Stress", main = "NMDS stress plot")
-  for (i in 1:10) {
-    points(rep(i + 1,10),replicate(10, metaMDS(x, 
-                                               autotransform = F, 
-                                               k = i + 1)$stress))
-  }
-}
-
-# Use the function that we just defined to choose the optimal nr of dimensions
-NMDS.scree(dist)
-set.seed(2)
-
-# Here, we perform the final analysis and check the result
-NMDS1 <- metaMDS(dist, k = 2, trymax = 100, trace = F)
-# Do you know what the trymax = 100 and trace = F means?
-# Let's check the results
-NMDS1
-
-# If you don`t provide a dissimilarity matrix, metaMDS automatically applies Bray-Curtis. So in our case, the results would have to be the same
-NMDS2 <- metaMDS(varespec, k = 2, trymax = 100, trace = F)
-NMDS2
-stressplot(NMDS2)
-plot(NMDS1, type = "t")
-NMDS3 <- metaMDS(varespec, k = 2, trymax = 100, trace = F, 
-                 autotransform = FALSE, distance="bray")
-plot(NMDS3)
-plot(NMDS3, display = "sites", type = "n")
-points(NMDS3, display = "sites", col = "red", cex = 1.25)
-text(NMDS3, display ="species")
-ordiplot(NMDS3, type = "n")
-orditorp(NMDS3, display = "species", col = "red", air = 0.01)
-orditorp(NMDS3, display = "sites", cex = 1.1, air = 0.01)
-
-stressplot(NMDS2)
-
-# Load the second dataset
-data(varechem)
-
-# The function envfit will add the environmental variables as vectors to the ordination plot
-ef <- envfit(NMDS3, varechem, permu = 999)
-ef
-
-# The two last columns are of interest: the squared correlation coefficient and the associated p-value
-# Plot the vectors of the significant correlations and interpret the plot
-plot(NMDS3, type = "t", display = "sites")
-plot(ef, p.max = 0.05)
-# Define a group variable (first 12 samples belong to group 1, last 12 samples to group 2)
-group = c(rep("Group1", 12), rep("Group2", 12))
-
-# Create a vector of color values with same length as the vector of group values
-colors = c(rep("red", 12), rep("blue", 12))
-
-# Plot convex hulls with colors based on the group identity
-ordiplot(NMDS3, type = "n")
-for(i in unique(group)) {
-  ordihull(NMDS3$point[grep(i, group),], draw="polygon",
-           groups = group[group == i],col = colors[grep(i,group)],label=F) } 
-
-orditorp(NMDS3, display = "species", col = "red", air = 0.01)
-orditorp(NMDS3, display = "sites", col = c(rep("red",12),
-                                           rep("blue", 12)), air = 0.01, cex = 1.25)
-
-
 
