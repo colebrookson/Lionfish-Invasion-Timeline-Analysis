13d9e4114e7ddf0eed958a852f151a56badec54a
Cole B. Brookson <brookson@ualberta.ca>
2020-06-22 16:16:09 -0600

    fix nmds plot to have points of most vulnerable species

--- code/multivar_analyses.R
+++ code/multivar_analyses.R
@@ -54,17 +54,48 @@ nmds_k2_vis_data_vulner = data.frame(scores(nmds_jaccard_k2))
 nmds_k2_vis_data_vulner$species = rownames(nmds_k2_vis_data_vulner)  
 nmds_k2_vis_data_vulner$vulner_score = grouping_data$cat_vulnerab_score  
 
-split_nmds_k2_vulner = split(nmds_k2_vis_data_vulner, nmds_k2_vis_data_vulner$vulner_score)
+split_nmds_k2_vulner = split(nmds_k2_vis_data_vulner, 
+                             nmds_k2_vis_data_vulner$vulner_score)
 applied_nmds_k2_vulner = lapply(split_nmds_k2_vulner, function(df){
   df[chull(df), ]
 })
 combined_nmds_k2_vulner = do.call(rbind, applied_nmds_k2_vulner)
 
+#load in data on top most consumed species
+species_data = read_csv(here('./data/top_species_from_peake_etal_2018.csv'))
+species_data = species_data %>% 
+  filter(rank <= 5)
+point_vulner = nmds_k2_vis_data_vulner %>%
+  filter(species %in% species_data$species)
 
-nmds_k2_vulner_plot <- ggplot() + 
-  geom_polygon(data=combined_nmds_k2_vulner, aes(x = NMDS1, y = NMDS2, fill= fct_relevel(vulner_score, 'Low', 'Med', 'High'), 
-                   group=fct_relevel(vulner_score, 'Low', 'Med', 'High')), alpha=0.30) + # add the convex hulls
-  geom_point(data=combined_nmds_k2_vulner, aes(x=NMDS1,y=NMDS2, colour = fct_relevel(vulner_score, 'Low', 'Med', 'High')), size=2) + # add the point markers
+point_vulner$species = gsub('_', ' ', as.character(point_vulner$species))
+
+nmds_k2_vulner_plot 
+ggplot() + 
+  geom_polygon(data=combined_nmds_k2_vulner, 
+               aes(x = NMDS1, y = NMDS2, 
+                   fill= fct_relevel(vulner_score, 'Low', 'Med', 'High'), 
+                   group=fct_relevel(vulner_score, 'Low', 'Med', 'High')), 
+               alpha=0.30) + # add the convex hulls
+  geom_point(data=combined_nmds_k2_vulner, 
+             aes(x=NMDS1,y=NMDS2, 
+                 colour = fct_relevel(vulner_score, 'Low', 'Med', 'High')), 
+             size=2) + # add the point markers
+  geom_jitter(data=point_vulner, 
+             aes(x=NMDS1,y=NMDS2, 
+                 colour = fct_relevel(vulner_score, 'Low', 'Med', 'High')), 
+             size=3, shape = 15,
+             #width = 0.05, height = 0.05,
+             position = position_jitter(width = 0.03, height = 0.3, seed = 7)) + # add the point markers
+  geom_label(data = point_vulner, 
+            aes(x=NMDS1,y=NMDS2,
+                 label = species, 
+                colour = fct_relevel(vulner_score, 'Med', 'High')),
+                #fill= fct_relevel(vulner_score, 'Low', 'Med', 'High'), 
+                #group=fct_relevel(vulner_score, 'Low', 'Med', 'High')),
+            size = 4, vjust= 0, hjust = 0,
+            position = position_jitter(width = 0.03, height = 0.3, seed = 7),
+            show.legend = FALSE)+
   coord_equal() +
   theme_bw()  +
   theme(axis.text.x = element_blank(),  # remove x-axis text
@@ -78,25 +109,34 @@ nmds_k2_vulner_plot <- ggplot() +
         panel.grid.major = element_blank(),  #remove major-grid labels
         panel.grid.minor = element_blank(),  #remove minor-grid labels
         plot.background = element_blank()) +
-  scale_fill_manual('Vulnerability', values = c('#006994', '#ffd300', '#ca3433')) +
-  scale_colour_manual('Vulnerability', values = c('#006994', '#ffd300', '#ca3433'))
+  scale_fill_manual('Vulnerability', 
+                    values = c('#006994', '#ffd300', '#ca3433')) +
+  scale_colour_manual('Vulnerability', 
+                      values = c('#006994', '#ffd300', '#ca3433')) +
+  scale_colour_manual('Vulnerability', 
+                      values = c('#006994', '#ffd300', '#ca3433'))
+ 
  
 ## make plot for vulnerability
 nmds_k2_vis_data_diet = data.frame(scores(nmds_jaccard_k2)) 
 nmds_k2_vis_data_diet$species = rownames(nmds_k2_vis_data_diet)  
 nmds_k2_vis_data_diet$diet_study = grouping_data$diet_study 
 
-split_nmds_k2_diet = split(nmds_k2_vis_data_diet, nmds_k2_vis_data_diet$diet_study)
+split_nmds_k2_diet = split(nmds_k2_vis_data_diet, 
+                           nmds_k2_vis_data_diet$diet_study)
 applied_nmds_k2_diet = lapply(split_nmds_k2_diet, function(df){
   df[chull(df), ]
 })
 combined_nmds_k2_diet = do.call(rbind, applied_nmds_k2_diet)
 combined_nmds_k2_diet = combined_nmds_k2_diet %>% 
-  mutate(diet_cat = ifelse(combined_nmds_k2_diet$diet_study == 1, 'Present', 'Absent'))
+  mutate(diet_cat = ifelse(combined_nmds_k2_diet$diet_study == 1, 
+                           'Present', 'Absent'))
 
 nmds_k2_plot_diet <- ggplot(data=combined_nmds_k2_diet) + 
-  geom_polygon(aes(x = NMDS1, y = NMDS2, fill= fct_relevel(diet_cat, 'Absent', 'Present'), 
-                   group=fct_relevel(diet_cat, 'Absent', 'Present')), alpha=0.30) + # add the convex hulls
+  geom_polygon(aes(x = NMDS1, y = NMDS2, fill= fct_relevel(diet_cat, 
+                                                           'Absent', 'Present'), 
+                   group=fct_relevel(diet_cat, 'Absent', 'Present')), 
+               alpha=0.30) + # add the convex hulls
   geom_point(aes(x=NMDS1,y=NMDS2, colour = diet_cat), size=2) + # add the point markers
   coord_equal() +
   theme_bw()  +
@@ -111,8 +151,10 @@ nmds_k2_plot_diet <- ggplot(data=combined_nmds_k2_diet) +
         panel.grid.major = element_blank(),  #remove major-grid labels
         panel.grid.minor = element_blank(),  #remove minor-grid labels
         plot.background = element_blank()) +
-  scale_fill_manual('Presence in Diet Studies', values = c('#000080', '#ed2939')) +
-  scale_colour_manual('Presence in Diet Studies', values = c('#000080', '#ed2939'))
+  scale_fill_manual('Presence in Diet Studies', 
+                    values = c('#000080', '#ed2939')) +
+  scale_colour_manual('Presence in Diet Studies', 
+                      values = c('#000080', '#ed2939'))
 
 
 
