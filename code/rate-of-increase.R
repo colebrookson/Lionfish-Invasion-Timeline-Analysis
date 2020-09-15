a8388d7bd95aa303842606cc7998559650305608
Cole Brookson <cole.brookson@gmail.com>
2020-09-15 14:41:06 -0600

    minimum year invaded calculation

--- code/rate-of-increase.R
+++ code/rate-of-increase.R
@@ -28,3 +28,46 @@ reef_abund_lf = reef_abund_full %>%
 #make single date column
 reef_abund_lf$date = as.Date(with(reef_abund_lf, paste(year, month, day, sep = '-')), 
                              '%Y-%m-%d')
+
+#find first observation in each subregion
+reef_abund_lf_minmax = reef_abund_lf %>% 
+  group_by(subregion) %>%
+  filter(Abundance > 0) %>% 
+  summarize(min_date = min(date))
+
+#NOTE --- decided to exclude any subregions which were invaded after 2015 (will do this later in the script)
+
+#find first largest observation
+reef_abund_lf_minmax = reef_abund_lf %>% 
+  group_by(subregion) %>%
+  filter(Abundance > 0) %>% 
+  summarize(min_date = min(date),
+            max_abund = max(Abundance)) %>% 
+  filter(min_date < '2015-01-01') %>%  #this isn't technically needed but keeping it for posterity
+  filter(max_abund > 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
