5dcf8eba3493e6c46271201f73d4352ebe5b102d
Cole B. Brookson <brookson@ualberta.ca>
2020-09-14 21:23:30 -0600

    intro front stuff

---
+++ code/rate-of-increase.R
@@ -0,0 +1,18 @@
+########## 
+##########
+# This code contains the analysis for the rate of change estimation presented
+# in Linardich et al. (2020) 
+# "Trait-based vulnerability framework reveals the impact of a global marine invader" 
+# This is the first of n code files for this analysis
+##########
+##########
+# AUTHOR: Cole B. Brookson
+# DATE OF CREATION: 2020-09-14
+##########
+#########
+
+library(tidyverse)
+library(here)
+
+
+
