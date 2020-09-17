a025c241ff1988f0c64b3fd8f271b88ce4081231
Cole Brookson <cole.brookson@gmail.com>
2020-09-17 12:59:15 -0600

    change colour of labels on nmds plot

--- code/rank_correlation_test.R
+++ code/rank_correlation_test.R
@@ -5,6 +5,12 @@
 # "Trait-based vulnerability framework reveals the impact of a global marine invader" 
 # This is the first of n code files for this analysis
 ##########
+########## 
+##########
+# This code contains the analysis for the rank abundance test presented in 
+# in Linardich et al. (2020) 
+# "Trait-based vulnerability framework reveals the impact of a global marine invader" 
+##########
 ##########
 # AUTHOR: Cole B. Brookson
 # DATE OF CREATION: 2020-06-15
@@ -14,7 +20,6 @@
 library(tidyverse)
 library(PNWColors)
 library(here)
-library(glmmTMB)
 `%notin%` = Negate(`%in%`)
 
 rank_data = read_csv(here('./data/diet_vulnerability_rank_corr_data.csv'))
@@ -28,8 +33,7 @@ rank_data$Species = trimws(rank_data$Species, which = c('left'))
 rank_data = merge(rank_data, vulner_data,
                     by.x = 'Species', by.y = 'binomial')
 
-hist(rank_data$FOO)
 
-#run  regression on the Frequency of Occurence continuous response variable
-rank_data$cum_vulnerab_score = as.factor(rank_data$cum_vulnerab_score)
+
+
 
